b7e6d8db90ef67d0ae3e1dbb58201a4f6a7f4a8b
Tony Hong <tony.hong@oocl.com>
2019-04-25 12:41:36 +0800

    1、编写队列的发布和订阅

--- src/main/java/com/oocl/springbootdemo/SpringbootDemoApplication.java
+++ src/main/java/com/oocl/springbootdemo/SpringbootDemoApplication.java
@@ -1,5 +1,6 @@
 package com.oocl.springbootdemo;
 
+import com.solacesystems.jcsmp.JCSMPException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
@@ -7,36 +8,35 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.jms.annotation.JmsListener;
 import org.springframework.messaging.Message;
 import org.springframework.messaging.MessageHeaders;
-import org.springframework.stereotype.Component;
 
 import java.util.*;
 
 @SpringBootApplication
 public class SpringbootDemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringbootDemoApplication.class, args);
-	}
+    public static void main(String[] args) throws JCSMPException {
+        SpringApplication.run(SpringbootDemoApplication.class, args);
+    }
 
-	@Component
-	static class MessageHandler {
+    //    @Component
+    static class MessageHandler {
 
-		private static final Logger logger = LoggerFactory.getLogger(MessageHandler.class);
+        private static final Logger logger = LoggerFactory.getLogger(MessageHandler.class);
 
 		// Retrieve the name of the queue from the application.properties file
-		@JmsListener(destination = "${solace.jms.demoQueueName}")
-		public void processMsg(Message msg) {
-			StringBuffer msgAsStr = new StringBuffer("============= Received \nHeaders:");
-			MessageHeaders hdrs = msg.getHeaders();
-			msgAsStr.append("\nUUID: " + hdrs.getId());
-			msgAsStr.append("\nTimestamp: " + hdrs.getTimestamp());
-			Iterator<String> keyIter = hdrs.keySet().iterator();
-			while (keyIter.hasNext()) {
-				String key = keyIter.next();
-				msgAsStr.append("\n" + key + ": " + hdrs.get(key));
-			}
-			msgAsStr.append("\nPayload: " + msg.getPayload());
-			logger.info(msgAsStr.toString());
-		}
-	}
+        @JmsListener(destination = "${solace.jms.demoQueueName}")
+        public void processMsg(Message msg) {
+            StringBuffer msgAsStr = new StringBuffer("============= Received \nHeaders:");
+            MessageHeaders hdrs = msg.getHeaders();
+            msgAsStr.append("\nUUID: " + hdrs.getId());
+            msgAsStr.append("\nTimestamp: " + hdrs.getTimestamp());
+            Iterator<String> keyIter = hdrs.keySet().iterator();
+            while (keyIter.hasNext()) {
+                String key = keyIter.next();
+                msgAsStr.append("\n" + key + ": " + hdrs.get(key));
+            }
+            msgAsStr.append("\nPayload: " + msg.getPayload());
+            logger.info(msgAsStr.toString());
+        }
+    }
 }
--- src/main/java/com/oocl/springbootdemo/config/ConsumerConfiguration.java
+++ src/main/java/com/oocl/springbootdemo/config/ConsumerConfiguration.java
@@ -1,7 +1,9 @@
 package com.oocl.springbootdemo.config;
 
+import com.solacesystems.jcsmp.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jms.annotation.EnableJms;
@@ -17,7 +19,11 @@ import java.io.*;
 public class ConsumerConfiguration {
 
     private static final Logger logger = LoggerFactory.getLogger(ConsumerConfiguration.class);
-
+    @Value("${solace.jms.host}") private String hostname;
+    @Value("${solace.jms.msgVpn}") private String vpnName;
+    @Value("${solace.jms.clientUsername}") private String username;
+    @Value("${solace.jms.clientPassword}") private String password;
+    @Value("${solace.jms.demoQueueName}") private String queueName;
     // Example configuration of the ConnectionFactory: we instantiate it here ourselves and set an error handler
     @Bean
     public DefaultJmsListenerContainerFactory cFactory(ConnectionFactory connectionFactory, DemoErrorHandler errorHandler) {
@@ -27,6 +33,41 @@ public class ConsumerConfiguration {
         return factory;
     }
 
+    @Bean
+    public Topic topic1() {
+        return JCSMPFactory.onlyInstance().createTopic("tutorial/topic");
+    }
+
+    @Bean
+    public JCSMPSession session(Topic topic) throws JCSMPException {
+        final JCSMPProperties properties = new JCSMPProperties();
+        properties.setProperty(JCSMPProperties.HOST, hostname);
+        properties.setProperty(JCSMPProperties.USERNAME, username);
+        properties.setProperty(JCSMPProperties.VPN_NAME, vpnName);
+        properties.setProperty(JCSMPProperties.PASSWORD, password);
+        JCSMPSession session = JCSMPFactory.onlyInstance().createSession(properties);
+        session.connect();
+        final XMLMessageConsumer cons = session.getMessageConsumer(new XMLMessageListener() {
+            @Override
+            public void onReceive(BytesXMLMessage msg) {
+                if (msg instanceof TextMessage) {
+                    System.out.printf("TextMessage received: '%s'%n", ((TextMessage)msg).getText());
+                } else {
+                    System.out.println("Message received.");
+                }
+                System.out.printf("Message Dump:%n%s%n", msg.dump());
+                //                latch.countDown();  // unblock main thread
+            }
+
+            @Override
+            public void onException(JCSMPException e) {
+                System.out.printf("Consumer received exception: %s%n", e);
+            }
+        });
+        session.addSubscription(topic);
+        cons.start();
+        return session;
+    }
     @Service
     public class DemoErrorHandler implements ErrorHandler {
 
