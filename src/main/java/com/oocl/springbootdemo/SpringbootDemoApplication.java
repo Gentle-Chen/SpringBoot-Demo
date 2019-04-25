6cc64623b89a6a7b6f927860af669c1f7220cc91
Tony Hong <tony.hong@oocl.com>
2019-04-25 11:05:19 +0800

    1.将hostname等配置化 2.将consummerListener注册成bean

--- src/main/java/com/oocl/springbootdemo/SpringbootDemoApplication.java
+++ src/main/java/com/oocl/springbootdemo/SpringbootDemoApplication.java
@@ -1,7 +1,15 @@
 package com.oocl.springbootdemo;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.jms.annotation.JmsListener;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageHeaders;
+import org.springframework.stereotype.Component;
+
+import java.util.*;
 
 @SpringBootApplication
 public class SpringbootDemoApplication {
@@ -10,4 +18,25 @@ public class SpringbootDemoApplication {
 		SpringApplication.run(SpringbootDemoApplication.class, args);
 	}
 
+	@Component
+	static class MessageHandler {
+
+		private static final Logger logger = LoggerFactory.getLogger(MessageHandler.class);
+
+		// Retrieve the name of the queue from the application.properties file
+		@JmsListener(destination = "${solace.jms.demoQueueName}")
+		public void processMsg(Message msg) {
+			StringBuffer msgAsStr = new StringBuffer("============= Received \nHeaders:");
+			MessageHeaders hdrs = msg.getHeaders();
+			msgAsStr.append("\nUUID: " + hdrs.getId());
+			msgAsStr.append("\nTimestamp: " + hdrs.getTimestamp());
+			Iterator<String> keyIter = hdrs.keySet().iterator();
+			while (keyIter.hasNext()) {
+				String key = keyIter.next();
+				msgAsStr.append("\n" + key + ": " + hdrs.get(key));
+			}
+			msgAsStr.append("\nPayload: " + msg.getPayload());
+			logger.info(msgAsStr.toString());
+		}
+	}
 }
